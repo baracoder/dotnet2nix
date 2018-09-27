ddc3f38cf0a53da3ce9129be407b1ecb3b3de9ea
Herman Fries <h.fries@nyris.io>
2018-09-27 15:46:14 +0200

    Extract builder to own function

---
+++ build-dotnet.nix
@@ -0,0 +1,83 @@
+{ fetchurl, dotnet-sdk, stdenv }:
+{
+  baseName,
+  version,
+  src,
+  feedUrlsFile ? ./urls.json
+  , defaultFeedUrl ? "https://www.nuget.org/api/v2"
+  , netrc-file ? null
+}:
+let
+    myFetchurl = if netrc-file == null
+                 then fetchurl
+                 else args: fetchurl (args // {
+                      curlOpts = "--netrc-file ${netrc-file}";
+                    });
+    feedUrls = if builtins.pathExists feedUrlsFile
+               then builtins.fromJSON (builtins.readFile feedUrlsFile )
+               else [ defaultFeedUrl ];
+    getUrls = baseName: version: urls:
+              if urls != null
+              then urls
+              else map (base: "${base}/package/${baseName}/${version}" ) feedUrls;
+    fetchNuPkg = ({
+      baseName
+      , version
+      , urls ? null
+      , sha512
+      }: myFetchurl {
+          inherit sha512;
+          urls = getUrls baseName version urls;
+          name = "${baseName}.${version}.nupkg";
+    });
+    nugetInfos = builtins.fromJSON (builtins.readFile ./nugets.json );
+    nugetsList = map (n: fetchNuPkg n) nugetInfos;
+in
+stdenv.mkDerivation rec {
+  name = "${baseName}-${version}";
+  buildInputs =  [ dotnet-sdk ];
+  nugets = map (n: "${n}:${n.name}") nugetsList;
+  src = ./. ;
+  buildPhase = ''
+    runHook preBuild
+
+    export DOTNET_CLI_TELEMETRY_OPTOUT=true
+    export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+    # avoid permission denied error
+    export HOME=$PWD
+
+    mkdir packages
+    cd packages
+    for p in $nugets; do
+      pstore=$(echo $p|cut -f1 -d:)
+      plink=$(echo $p|cut -f2 -d:)
+      ln -s $pstore $plink
+    done
+    cd ..
+
+    echo Running dotnet restore
+    dotnet restore --source $PWD/packages
+    echo Running dotnet build
+    dotnet build --no-restore
+
+    runHook postBuild
+  '';
+  dontStrip = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    echo Running dotnet publish
+    dotnet publish --no-restore --no-build -o $out
+
+    echo Creating wrapper
+    mkdir $out/bin
+    cat << EOF >> $out/bin/${baseName}
+    #!/bin/sh
+    ${dotnet-sdk}/bin/dotnet $out/${baseName}.dll -- "$@"
+    EOF
+    chmod +x $out/bin/${baseName}
+    runHook postInstall
+  '';
+
+}
--- default.nix
+++ default.nix
@@ -1,85 +1,13 @@
 {
   pkgs ? import <nixpkgs> {}
-  , feedUrlsFile ? ./urls.json
-  , defaultFeedUrl ? "https://www.nuget.org/api/v2"
-  , netrc-file ? null
 }:
 let
-    fetchurl = if netrc-file == null
-               then pkgs.fetchurl
-               else args: pkgs.fetchurl (args // {
-                    curlOpts = "--netrc-file ${netrc-file}";
-               });
-    feedUrls = if builtins.pathExists feedUrlsFile
-               then builtins.fromJSON (builtins.readFile feedUrlsFile )
-               else [ defaultFeedUrl ];
-    getUrls = baseName: version: urls:
-              if urls != null
-              then urls
-              else map (base: "${base}/package/${baseName}/${version}" ) feedUrls;
-    fetchNuPkg = ({
-      baseName
-      , version
-      , urls ? null
-      , sha512
-      }: fetchurl {
-          inherit sha512;
-          urls = getUrls baseName version urls;
-          name = "${baseName}.${version}.nupkg";
-    });
-    nugetInfos = builtins.fromJSON (builtins.readFile ./nugets.json );
-    nugetsList = map (n: fetchNuPkg n) nugetInfos;
+    builder = pkgs.callPackage ./build-dotnet.nix {};
 in
 {
-    dotnet2nix = pkgs.stdenv.mkDerivation rec {
-        baseName = "dotnet2nix";
-        version = "2018.1";
-        name = "${baseName}-${version}";
-        buildInputs =  [ pkgs.dotnet-sdk ];
-        nugets = map (n: "${n}:${n.name}") nugetsList;
-        src = ./. ;
-        buildPhase = ''
-            runHook preBuild
-
-            export DOTNET_CLI_TELEMETRY_OPTOUT=true
-            export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-            # avoid permission denied error
-            export HOME=$PWD
-
-            mkdir packages
-            cd packages
-            for p in $nugets; do
-              pstore=$(echo $p|cut -f1 -d:)
-              plink=$(echo $p|cut -f2 -d:)
-              ln -s $pstore $plink
-            done
-            cd ..
-
-            echo Running dotnet restore
-            dotnet restore --source $PWD/packages
-            echo Running dotnet build
-            dotnet build --no-restore
-
-            runHook postBuild
-        '';
-        dontStrip = true;
-
-        installPhase = ''
-            runHook preInstall
-
-            echo Running dotnet publish
-            dotnet publish --no-restore --no-build -o $out
-
-            echo Creating wrapper
-            mkdir $out/bin
-            cat << EOF >> $out/bin/dotnet2nix
-            #!/bin/sh
-            ${pkgs.dotnet-sdk}/bin/dotnet $out/dotnet2nix.dll -- "$@"
-            EOF
-            chmod +x $out/bin/dotnet2nix
-            runHook postInstall
-        '';
-
+    dotnet2nix = builder {
+      baseName = "dotnet2nix";
+      version = "2018.1";
+      src = ./. ;
     };
 }
-
