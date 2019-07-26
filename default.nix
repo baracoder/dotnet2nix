0c261d61e6d444c7bdbba3faeb9324548859af1a
Herman Fries <baracoder@googlemail.com>
2019-07-26 15:50:03 +0200

    Pin nixpkgs version

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 {
-   pkgs ? import <nixpkgs> {}
+  pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs-channels/archive/nixos-19.03.tar.gz) {}
 }:
 let
   builder = pkgs.callPackage ./build-dotnet.nix {};
